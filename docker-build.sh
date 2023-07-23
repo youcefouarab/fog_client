2edc3efb7770cda03f4391f61839292600a4aa1c
Youcef OUARAB <hy_ouarab@esi.dz>
2023-07-23 09:31:58 +0100

    fixed some requirements

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
-sudo docker build --tag fog_client:latest .
-sudo docker build --tag fog_client:containernet --file Dockerfile-containernet . 
-sudo docker build --tag fog_client:script --file Dockerfile-script .
-sudo docker build --tag fog_client:containernet-script --file Dockerfile-containernet-script . 
+#!/bin/bash
+sudo docker build --no-cache --tag fog_client:latest .
+sudo docker build --no-cache --tag fog_client:containernet --file Dockerfile-containernet . 
+sudo docker build --no-cache --tag fog_client:script --file Dockerfile-script .
+sudo docker build --no-cache --tag fog_client:containernet-script --file Dockerfile-containernet-script . 
